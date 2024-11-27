4d75d54751ed08a164132726ddaeb049104c7c22
noahstafford <noahstafford12@gmail.com>
2024-11-27 16:59:52 -0500

    character class functions should all be in there now

--- Character.cpp
+++ Character.cpp
@@ -1,31 +1,64 @@
-#ifndef CHARACTER_H
-#define CHARACTER_H
 #include <iostream>
 #include <string>
+#include "Character.h"
 
 using namespace std;
 
-class Character
+Character::Character(string name, int level, int health, int attack, int experience, int critical_hit_chance) 
 {
-    public:
-        Character (string name, int level, int health, int attack, int experience);
-        void level_up();
-        int attack();
-        int defend();
-        void get_level();
-        void set_level();
-        void get_health();
-        void set_health();
-        void get_attack();
-        void set_attack();
-        void get_experience();
-        void set_experience();
-    private:
-        string name;
-        int level;
-        int health;
-        int attack;
-        int experience;
-};
+    this->name = name;
+    this->level = level;
+    this->health = health;
+    this->attack = attack;
+    this->experience = experience;
+    this->critical_hit_chance = critical_hit_chance;
+}
 
-#endif
+void Character::level_up() {
+    if (experience >= experience_to_level_up) {
+        level++;
+        experience = 0;
+        experience_to_level_up += (experience_to_level_up * .25);
+        critical_hit_chance++;
+    } 
+}
+
+int Character::attack_enemy() {
+    return attack; //this will eventually factor in critical hits and missing and stuff
+}
+
+int Character::defend(int damage_taken) {
+    return 1;
+}
+
+int Character::take_damage(int damage_taken){
+    return 1;
+}
+
+int Character::heal(int healing_taken){
+    return 1;
+}
+
+int Character::get_level() {
+    return level;
+}
+
+int Character::get_health() {
+    return health;
+}
+
+void Character::set_health(int new_health) {
+    health = new_health;
+}
+
+void Character::set_attack(int attack_power) {
+    attack = attack_power;
+}
+
+int Character::get_experience() {
+    return experience;
+}
+
+void Character::set_experience(int experience_gained) {
+    experience += experience_gained;
+}
--- Character.h
+++ Character.h
@@ -8,24 +8,30 @@ using namespace std;
 class Character
 {
     public:
-        Character (string name, int level, int health, int attack, int experience);
+        Character (string name, int level, int health, int attack, int experience, int critical_hit_chance);
         void level_up();
-        int attack();
-        int defend();
-        void get_level();
+        int attack_enemy();
+        int defend(int damage_taken);
+        int take_damage(int damage_taken);
+        int heal(int healing_taken);
+        int get_level();
         void set_level();
-        void get_health();
-        void set_health();
-        void get_attack();
-        void set_attack();
-        void get_experience();
-        void set_experience();
+        int get_health();
+        void set_health(int new_health);
+        int get_attack();
+        void set_attack(int attack_power);
+        int get_experience();
+        void set_experience(int experience_gained);
+        int get_experience_to_level_up();
+        void set_experience_to_level_up();
     private:
         string name;
         int level;
         int health;
         int attack;
+        int critical_hit_chance;
         int experience;
+        int experience_to_level_up;
 };
 
 #endif
