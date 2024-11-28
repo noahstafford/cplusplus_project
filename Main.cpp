3a53c9c0d18aeb32ba3a7586a5e029f0a353165a
noahstafford <noahstafford12@gmail.com>
2024-11-27 19:46:39 -0500

    created a main

---
+++ Main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include "Character.h"
+#include "Dungeon.h"
+#include "Monster.h"
+
+using namespace std;
+
+int main() {
+
+    string name;
+    cout << "Enter a name for your character: "; // Type a number and press enter
+    cin >> name;
+    cout << "Your name is " + name;
+    //Character player(name, 0, 100, 10, 10, 0, 1);
+
+}
