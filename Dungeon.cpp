0beea3be923b7ec621a1decb0c515d9ff94e5ad7
noahstafford <noahstafford12@gmail.com>
2024-12-04 18:38:02 -0500

    working on creating a room with monsters and random event stuffs

--- Dungeon.cpp
+++ Dungeon.cpp
@@ -11,9 +11,11 @@ Dungeon::Dungeon(int num_of_rooms, int difficulty) {
 }
 
 
-void Dungeon::create_room() {
+void Dungeon::create_room(string choice) {
+    cout << random_description() << endl;
+    spawn_monster();
+    create_encounter();
     
-
 }
 
 void Dungeon::create_encounter() {
@@ -33,7 +35,7 @@ void Dungeon::select_next_room() {
     string choice = "";
     bool validchoice = false;
 
-    while (choice != "w" && choice != "n" && choice != "e" && choice != "u" && choice != "d" && validchoice == false) {
+    while (!validchoice) {
         cout << "It is time to select a new room to traverse." << endl;
         for (int x = 0; x < rooms.size(); x++) {
             cout << rooms[x] << endl;
@@ -41,8 +43,19 @@ void Dungeon::select_next_room() {
 
         cout << "What is your choice? ";
         cin >> choice;
-        validate_choice(choice, rooms);
+        validchoice = validate_choice(choice, rooms);
 }
+
+    if (choice == "u") {
+        cout << "You climb the ladder. It falls behind you, leaving you trapped." << endl;
+    }
+    else if (choice == "d") {
+        cout << "You drop down into the room below you." << endl;
+    }
+    else {
+        cout << "You enter the room and the door slams shut behind you." << endl;
+    }
+    create_room(choice);
 
 }
 
@@ -54,15 +67,17 @@ bool Dungeon::validate_choice(string choice, vector<string> rooms) {
     else if (choice == "n" && rooms.size() >= 1) {
         return true;
     }
-    else if (choice == "e" && rooms.size() >= 3) {
+    else if (choice == "e" && rooms.size() >= 2) {
         return true;
     }
-    else if (choice == "u" && rooms.size() >= 4) {
+    else if (choice == "u" && rooms.size() >= 3) {
         return true;
     }
-    else if (choice == "d" && rooms.size() >= 5) {
+    else if (choice == "d" && rooms.size() >= 4) {
         return true;
     }
+
+    return false;
 }
 
 vector<string> Dungeon::generate_dungeon() {
@@ -85,4 +100,41 @@ vector<string> Dungeon::generate_dungeon() {
 
     return rooms;
     //not the MOST random or the MOST efficent and doesn't make the MOST sense but it works
+}
+
+string Dungeon::random_description() {
+    int num = rand() % 11;
+    if (num == 0) {
+        return "The room is pitch black";
+    }
+    else if (num == 1) {
+        return "The room is pitch black";
+    }
+    else if (num == 2) {
+        return "The room is pitch black";
+    }
+    else if (num == 3) {
+        return "The room is pitch black";
+    }
+    else if (num == 4) {
+        return "The room is pitch black";
+    }
+    else if (num == 5) {
+        return "The room is pitch black";
+    }
+    else if (num == 6) {
+        return "The room is pitch black";
+    }
+    else if (num == 7) {
+        return "The room is pitch black";
+    }
+    else if (num == 8) {
+        return "The room is pitch black";
+    }
+    else if (num == 9) {
+        return "The room is pitch black";
+    }
+    else if (num == 10) {
+        return "The room is pitch black";
+    }
 }
--- Dungeon.h
+++ Dungeon.h
@@ -2,6 +2,7 @@
 #define DUNGEON_H
 #include <iostream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
@@ -9,12 +10,14 @@ class Dungeon
 {
     public:
         Dungeon(int num_of_rooms, int difficulty);
-        void create_room();
+        void create_room(string choice);
         void create_encounter();
         void spawn_monster();
         void random_event();
         vector<string> generate_dungeon();
+        bool validate_choice(string choice, vector<string> rooms);
         void select_next_room();
+        string random_description();
     private:
         string room_description;
         int num_of_rooms;
